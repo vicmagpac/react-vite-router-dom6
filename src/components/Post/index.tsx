09050d730ed5f5f387f727e42bd7042e138ecef1
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2023-01-09 09:34:38 -0300

    add return query string params

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,12 +1,14 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import './style.css';
 
 export const Post = () => {
     const params = useParams();
     const { id } = params;
+    const [ queryString ] = useSearchParams();
+
     return(
         <div>
-            <h1>Posts {id}</h1>
+            <h1>Posts {`Para id: ${id}`} - {`QueryString: ${queryString.get('page')}`}</h1>
         </div>
     );
 };
