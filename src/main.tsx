e74c06dc49c17548c9ab40069b071187e1fbdebe
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2023-01-09 09:02:43 -0300

    add component about and route mapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './components/Home'
+import { About } from './components/About'
 import './styles/global.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          element={<Home />}
+          />
+        <Route 
+          path='about'
+          element={<About />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
